0589551b99f4387afa474a6dfe6ebdc69cd2f6b3
That Guy Vic <vicsergeevrov@icloud.com>
2024-01-05 18:47:41 +0300

    structure added

---
+++ ABV-Calculator/DataStore/DataStore.swift
@@ -0,0 +1,8 @@
+//
+//  DataStore.swift
+//  ABV-Calculator
+//
+//  Created by Vic on 05.01.2024.
+//
+
+import Foundation
---
+++ ABV-Calculator/Models/Value.swift
@@ -0,0 +1,8 @@
+//
+//  Value.swift
+//  ABV-Calculator
+//
+//  Created by Vic on 05.01.2024.
+//
+
+import Foundation
