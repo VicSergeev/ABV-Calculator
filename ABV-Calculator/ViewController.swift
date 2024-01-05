deda1cad9d08f6032188dbfc875a7be61d2ca578
That Guy Vic <vicsergeevrov@icloud.com>
2024-01-05 18:43:46 +0300

    project created

---
+++ ABV-Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ABV-Calculator
+//
+//  Created by Vic on 05.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
