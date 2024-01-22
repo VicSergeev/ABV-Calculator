ec380947f1788da34a5c0c73470a96224aa9f796
That Guy Vic <vicsergeevrov@icloud.com>
2024-01-22 22:45:27 +0300

    Update UserInterfaceState.xcuserstate

--- ABV-Calculator/Models/Developer.swift
+++ ABV-Calculator/Models/Developer.swift
@@ -6,5 +6,33 @@
 //
 
 struct Developer {
-    // модель для AboutVC
+    let name: String
+    let lastname: String
+    
+    var fullName: String {
+        "\(name) \(lastname)"
+    }
+    
+    static func getDevNames() -> [Developer] {
+        var devNames: [Developer] = []
+        let devData = DeveloperDataStore.shared
+        
+        let names = devData.names
+        let lastnames = devData.lastnames
+        
+        let iterationsCount = min(
+            names.count,
+            lastnames.count
+        )
+        
+        for index in 0..<iterationsCount {
+            let devName = Developer(
+                name: names[index],
+                lastname: lastnames[index]
+            )
+            
+            devNames.append(devName)
+        }
+        return devNames
+    }
 }
--- ABV-Calculator/View Controllers/AboutViewController.swift
+++ ABV-Calculator/View Controllers/AboutViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 final class AboutViewController: UIViewController {
-
-    // о команде
-    // краткое описание
+    private var devIndex = 0
+    private var developers = Developer.getDevNames()
+    
+    @IBOutlet var devNamesLabels: [UILabel]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getNames()
     }
+}
 
-
+// MARK: - Private methods
+private extension AboutViewController {
+    func getNames() {
+        for index in 0..<devNamesLabels.count {
+            let dev = developers[devIndex]
+            let labelIndex = devNamesLabels[index]
+            labelIndex.text = dev.fullName
+            devIndex += 1
+        }
+    }
 }
